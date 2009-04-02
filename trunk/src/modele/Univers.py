7a6842728555630391b2cc47bf80287b3dda8b5d
eliana.duque <eliana.duque@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-04-02 21:40:51 +0000

    modif Eliana

--- trunk/src/modele/Univers.py
+++ trunk/src/modele/Univers.py
@@ -1,16 +1,20 @@
 #l'univers
-class Univers(object):
+class Univers(Object):
     def __init__(self):
+        self.tailleX=500
+        self.tailleY=600
         self.joueurs = []
         self.systemes = []
+        self.couleurscivs=["blue","green","purple","orange","yellow", "red"]    # Une couleur pour chaque joueur
+        ## creer 10 systemes
         
     def ajouterJoueur(self):
         vaisseaux = []
         self.joueurs.append(vaisseaux)
         return len(self.joueurs)-1
         
-    def ajouterVaisseau(self, joueur, vaisseau):
-        self.joueurs[joueur].append(vaisseau)
+#    def ajouterVaisseau(self, joueur, vaisseau): ## Les vaisseaux appartienent aux joueurs
+#        self.joueurs[joueur].append(vaisseau)
         
     def ajouterSysteme(self, systeme):
         self.systemes.append(systeme)
