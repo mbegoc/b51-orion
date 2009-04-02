8d324d56e98390697f8b5c3f6658d13e2b92d659
eliana.duque <eliana.duque@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-04-02 21:38:10 +0000

    modif Eliana

---
+++ trunk/src/modele/Joueur.py
@@ -0,0 +1,17 @@
+class Joueur(Object):
+    def __init__(self, parent, id, coleur, systInit):
+        self.parent=parent
+        self.couleur = couleur
+        self.id = id
+        self.vaisseaux = []
+        self.systemes = []
+        self.ajouterSysteme(systInit)
+        self.ajouterVaisseau(systInit.posX, systInit.PosY)
+        
+    def ajouterSysteme(self, systeme):
+        self.systemes.append(systeme)      
+    
+    def ajouterVaisseau(self, posX, posY):
+        self.vaisseaux.append(Vaisseau(self,posX,posY))
+        
+  
