16d06c33ad6fd508ae16dc2d847c4620892aae10
michel.begoc <michel.begoc@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-04-02 21:04:34 +0000

    Ajout d'une vue qui propose une architecture modulaire: chaque partie de l'ecran est un objet qui herite d'un objet Tkinter et qui peut être manipulé comme n'importe quel objet Tkinter.

---
+++ src/client/Vue.py
@@ -0,0 +1,50 @@
+'''
+Created on 2009-04-02
+
+@author: Michel
+'''
+
+from Tkinter import *
+
+class Vue(object):
+    def __init__(self):
+        self.root = Tk()
+        
+        self.menuBas = MenuBas(self.root)
+        self.menuBas.grid(column=0, row=1)
+        
+        self.menuCote = MenuCote(self.root)
+        self.menuCote.grid(column=1, row=0)
+        
+        self.ssCanvas = SsCanvas(self.root)
+        self.ssCanvas.grid(column=0, row=0)
+        
+        self.root.mainloop()
+    
+class MenuBas(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.bouton1 = Button(self, text="Bouton 1")
+        self.bouton2 = Button(self, text="Bouton 2")
+        self.bouton3 = Button(self, text="Bouton 3")
+        self.bouton1.pack()
+        self.bouton2.pack()
+        self.bouton3.pack()
+    
+class MenuCote(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.bouton1 = Button(self, text="Bouton 1")
+        self.bouton2 = Button(self, text="Bouton 2")
+        self.bouton3 = Button(self, text="Bouton 3")
+        self.bouton1.pack()
+        self.bouton2.pack()
+        self.bouton3.pack()
+
+class SsCanvas(Canvas):
+    def __init__(self, root):
+        Canvas.__init__(self, root, width=200, height=200)
+        self.create_line(0, 0, 200, 200)
+        
+if __name__ == "__main__":
+    vue = Vue()
