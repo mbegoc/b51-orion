e4dd1a2b0187d154dd533052114b06438c1b152c
michel.begoc <michel.begoc@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-04-03 01:15:03 +0000

    Une petite mise à jour avec des scrollbars. Ce n'est vraiment pas difficile à implanter en python, alors ce sera peut-etre une bonne solution pour plus tard, lorsque notre univers deviendra très grand :) A voir...

--- src/client/Vue.py
+++ src/client/Vue.py
@@ -11,14 +11,23 @@ class Vue(object):
         self.root = Tk()
         
         self.menuBas = MenuBas(self.root)
-        self.menuBas.grid(column=0, row=1)
+        self.menuBas.grid(column=0, row=2)
         
         self.menuCote = MenuCote(self.root)
-        self.menuCote.grid(column=1, row=0)
+        self.menuCote.grid(column=2, row=0)
         
         self.ssCanvas = SsCanvas(self.root)
         self.ssCanvas.grid(column=0, row=0)
-        
+    
+        self.scrollY = Scrollbar(self.root, orient=VERTICAL, command=self.ssCanvas.yview)
+        self.scrollY.grid(row=0, column=1, sticky=N+S)
+    
+        self.scrollX = Scrollbar(self.root, orient=HORIZONTAL, command=self.ssCanvas.xview)
+        self.scrollX.grid ( row=1, column=0, sticky=E+W )
+    
+        self.ssCanvas["xscrollcommand"] = self.scrollX.set
+        self.ssCanvas["yscrollcommand"] = self.scrollY.set
+            
         self.root.mainloop()
     
 class MenuBas(Frame):
@@ -47,8 +56,10 @@ class MenuCote(Frame):
 
 class SsCanvas(Canvas):
     def __init__(self, root):
-        Canvas.__init__(self, root, width=200, height=200)
-        self.create_line(0, 0, 200, 200)
+        Canvas.__init__(self, root, width=1000, height=600, background="#000000", scrollregion=(0, 0, 2000, 2000))
+        self.create_line(0, 0, 2000, 2000, fill="#ffffff")
+        self.create_line(2000, 0, 0, 2000, fill="#ffffff")
+
         
 if __name__ == "__main__":
     vue = Vue()
