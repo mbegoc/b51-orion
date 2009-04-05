2002a226e11ac943f8b1436911584624fd1164c3
michel.begoc <michel.begoc@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-04-05 15:11:08 +0000

    Ajout du support des images. Les images contenues dans le repertoire "images" sont automatiquement chargees dans un dictionnaire dont les clefs sont le nom de base des images (sans extension). On peut ensuite appeler la fonction dessinerImage(nom, x, y).

--- trunk/src/client/Vue.py
+++ trunk/src/client/Vue.py
@@ -5,18 +5,21 @@ Created on 2009-04-02
 '''
 
 from Tkinter import *
+from PIL import Image, ImageTk
+import glob, os
 
 class Vue(object):
     def __init__(self):
         self.root = Tk()
+        self.root.title = "Galax"
         
-        self.menuBas = MenuBas(self.root)
+        self.menuBas = MenuBas(self)
         self.menuBas.grid(column=0, row=2)
         
-        self.menuCote = MenuCote(self.root)
+        self.menuCote = MenuCote(self)
         self.menuCote.grid(column=2, row=0)
         
-        self.ssCanvas = SsCanvas(self.root)
+        self.ssCanvas = SsCanvas(self)
         self.ssCanvas.grid(column=0, row=0)
     
         self.scrollY = Scrollbar(self.root, orient=VERTICAL, command=self.ssCanvas.yview)
@@ -27,12 +30,16 @@ class Vue(object):
     
         self.ssCanvas["xscrollcommand"] = self.scrollX.set
         self.ssCanvas["yscrollcommand"] = self.scrollY.set
-            
+
         self.root.mainloop()
     
 class MenuBas(Frame):
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
+    def __init__(self, mere):
+        #appel au constructeur de la super classe
+        Frame.__init__(self, mere.root)
+        #on garde une reference vers la classe mere
+        self.mere = mere
+        
         self.nom = Label(self, text="Menu du bas")
         self.bouton1 = Button(self, text="Bouton 1")
         self.bouton2 = Button(self, text="Bouton 2")
@@ -43,9 +50,14 @@ class MenuBas(Frame):
         self.bouton3.pack(side=LEFT)
     
 class MenuCote(Frame):
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-        self.bouton1 = Button(self, text="Bouton 1")
+    def __init__(self, mere):
+        #appel au constructeur de la super classe
+        Frame.__init__(self, mere.root)
+        #on garde une reference vers la classe mere
+        self.mere = mere
+        
+        self.bouton1 = Button(self, text="Dessiner jeu")
+        self.bouton1.bind("<Button-1>", self.representerJeu)
         self.bouton2 = Button(self, text="Bouton 2")
         self.bouton3 = Button(self, text="Bouton 3")
         self.nom = Label(self, text="Menu de cote")
@@ -53,13 +65,51 @@ class MenuCote(Frame):
         self.bouton1.pack()
         self.bouton2.pack()
         self.bouton3.pack()
+    
+    def representerJeu(self, evt):
+        self.mere.ssCanvas.representerJeu()
 
 class SsCanvas(Canvas):
-    def __init__(self, root):
-        Canvas.__init__(self, root, width=1000, height=600, background="#000000", scrollregion=(0, 0, 2000, 2000))
+    def __init__(self, mere):
+        #appel au constructeur de la super classe
+        Canvas.__init__(self, mere.root, width=1000, height=600, background="#000000", scrollregion=(0, 0, 2000, 2000))
+        #on garde une reference vers la classe mere
+        self.mere = mere
+        
+        #initialisation des images disponibles
+        self.images = {}
+        SsCanvas.__genererImages__(self)
+
         self.create_line(0, 0, 2000, 2000, fill="#ffffff")
         self.create_line(2000, 0, 0, 2000, fill="#ffffff")
-
+    
+    #cette fonction ne sert qu'a 
+    def dessinerImage(self, nom, x, y):
+        if nom in self.images:
+            self.create_image(x, y, image=self.images[nom])
+            return 1
+        else:
+            return 0
+    
+    '''ATTENTION: cette fonction doit imperativement etre appelee a travers la classe de l'objet, par par l'objet lui meme (SsCanvas.__genererImages__(objet) )
+    Je ne sais pas pourquoi, mais si l'image est generee dans l'objet sur lequel elle va etre affichee, ca ne marche pas. Il faut donc appeler cette methode
+    sur la classe et passer en parametre l'instance d'un SsCanvas dans lequel la liste d'images sera cree'''
+    #genere un dictionnaire des images contenues dans le repertoire images. La clef utilisee comme entree du dictionnaire est le nom de base du fichier (sans extension)
+    def __genererImages__(instance):
+        for file in glob.glob("images/*"):
+            image = Image.open(file)
+            photo = ImageTk.PhotoImage(image)
+            key = os.path.splitext(os.path.split(file)[1])[0]
+            instance.images[key] = photo
+        
+    def representerJeu(self):
+        self.dessinerImage("abasourdi", 115, 100)
+        self.dessinerImage("bise", 65, 100)
+        self.dessinerImage("cligne", 50, 170)
+        self.dessinerImage("etonne", 70, 190)
+        self.dessinerImage("prof", 90, 190)
+        self.dessinerImage("ricane", 110, 190)
+        self.dessinerImage("yeuxLeves", 130, 170)
         
 if __name__ == "__main__":
     vue = Vue()
