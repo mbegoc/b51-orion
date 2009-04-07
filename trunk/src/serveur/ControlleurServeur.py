0193bc0df8ffef470c8859bdba34cf7c4cb8e0b3
edolillo <edolillo@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-04-07 22:08:39 +0000

    Mod Eliana - eduardo (7Avr)

--- trunk/src/serveur/ControlleurServeur.py
+++ trunk/src/serveur/ControlleurServeur.py
@@ -1,23 +1,40 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import pickle
 import datetime
-
+import random
+from modele.Systeme import Systeme
+from modele.Univers import Univers
+from modele.Joueur import Joueur
 
 # Creer un serveur
 server = SimpleXMLRPCServer(("localhost", 8000),
                             requestHandler=SimpleXMLRPCRequestHandler)
-server.register_introspection_functions()
+
 
 # Enregistrer l'instance d'une classe
 #Toutes ses fonctions vont etre publiees
-class Serveur:
+class ControlleurServeur(object):
     def __init__(self):
+        self.idJoueur=0
+    def ConecterJoueur(self):
+        if(len(Univers.joueurs)==0):
+           for s in range(1,10):             
+              Systeme.x=random.randint(0,50)*10 
+              Systeme.y=random.randint(0,60)*10
+              if(Univers.systemes.count(Systeme)):
+                s=s-1
+              else:               
+                Univers.ajouterSysteme(Systeme)
+             
+    def AjouterSysteme(self,x,y):
+        Systeme.x=x
+        Systeme.y=y
         pass
-        
-    def uneFonction(self):
-        pass
-    
-server.register_instance(Serveur())
+
+
+
+server.register_instance(ControlleurServeur())
 
 # Demarrer la boucle du serveur
 print "OK, serveur demarrer"
