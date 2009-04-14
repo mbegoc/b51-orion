7c38b1314b6acaa43cfe6db62e29cc816c6108fc
gomorrha@gmail.com <gomorrha@gmail.com@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-04-14 21:52:50 +0000

    cree un deuxieme controleur. ajoute fonction dans univers pour generer 100 systemes. pour l'instant, le controleur ne fait qu'imprimer les coordonnees des systemes crees.

--- src/modele/Univers.py
+++ src/modele/Univers.py
@@ -1,13 +1,24 @@
 #l'univers
 from modele.Joueur import Joueur
+from modele.Systeme import Systeme
+
+import random
 
 class Univers(object):
     def __init__(self):
         self.tailleX=500
         self.tailleY=600
         self.joueurs = []
-        self.systemes = []
+        
         self.couleursJoueurs=["blue","green","purple","orange","yellow","red"]    # Une couleur pour chaque joueur
+        
+        self.nbrSystemes = 100 #nombre de systemes dans l'univers existant
+        self.systemes = range(self.nbrSystemes)#les systemes seront places dans cette variable
+        self.initUniverse()
+        
+    def initUniverse(self):
+        for x in range(self.nbrSystemes):#je vais changer ca pour un shuffle plus tard pour eviter les collisions             
+              self.systemes[x]=Systeme(random.randint(0,50)*10,random.randint(0,50)*10) 
         
     def ajouterJoueur(self,id):
         couleur=self.couleursJoueurs.pop()
