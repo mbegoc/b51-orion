25c3c41db5891f061219a4d72250c3d9bb5edd33
gomorrha <gomorrha@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-04-20 05:47:09 +0000

    modele/Chat.py Ce fichier contient les instructions pour faire fonctionner le chat. Les conversations sont sauvegardées dans un fichier chatlog.txt. Pour l'instant, le Miniclient ne fait qu'imprimer les messages, il ne les met pas dans une structures mais cela peut être changé plus tard si nécessaire.

---
+++ trunk/src/modele/Chat.py
@@ -0,0 +1,51 @@
+#/usr/bin/env python
+
+#ce fichier gere le chat
+
+import time
+
+class Chat(object):
+    def __init__(self):
+        self.chatlog = open("chatlog.txt", "w")
+        self.nbrMessage = 0
+
+        #les noms et les messages sont separes en deux listes. je n'ai pas trouve
+        #pour le moment de facon plus efficace de representer l'information...
+        self.nicks = []
+        self.messages =[]
+
+
+
+
+    def addMessage(self, nick, message):
+        self.nicks.append(nick)
+        self.messages.append(message)
+        
+        #ecrit le chat dans le log
+        self.chatlog.write(time.strftime("%H:%M:%S", time.gmtime()) + " <" +self.nicks[self.nbrMessage] + "> " + self.messages[self.nbrMessage] + "\n")
+
+        #debug
+        #print self.nicks[self.nbrMessage] + ":" + self.messages[self.nbrMessage]
+
+        self.nbrMessage = self.nbrMessage + 1
+
+
+    def currentMessage(self):
+        return self.nbrMessage
+
+    def returnMessage(self, numeroMsg):
+        self.transmission = [self.nicks[numeroMsg], self.messages[numeroMsg]]
+        return self.transmission
+
+
+    def terminer(self):
+        self.chatlog.close()
+
+'''
+leChat = Chat()
+
+leChat.ajouterMessage(nom, msg)
+leChat.terminer()
+
+print leChat.returnMsg(0)
+'''
