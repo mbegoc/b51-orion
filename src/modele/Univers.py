6c0b7e73be712851c877650a48b5d54a02e7f935
eliana.duque <eliana.duque@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-04-23 14:40:31 +0000

    Eliana 23Avr

--- src/modele/Univers.py
+++ src/modele/Univers.py
@@ -8,17 +8,19 @@ class Univers(object):
     def __init__(self):
         self.tailleX=500
         self.tailleY=600
-        self.joueurs = []
+        self.joueurs = {}
         
         self.couleursJoueurs=["blue","green","purple","orange","yellow","red"]    # Une couleur pour chaque joueur
+
+        self.systemes=[]
         
-        self.nbrSystemes = 100 #nombre de systemes dans l'univers existant
-        self.systemes = range(self.nbrSystemes)#les systemes seront places dans cette variable
-        self.initUniverse()
-        
-    def initUniverse(self):
-        for x in range(self.nbrSystemes):#je vais changer ca pour un shuffle plus tard pour eviter les collisions             
-              self.systemes[x]=Systeme(random.randint(0,50)*10,random.randint(0,50)*10) 
+#        self.nbrSystemes = 100 #nombre de systemes dans l'univers existant
+#        self.systemes = range(self.nbrSystemes)#les systemes seront places dans cette variable
+#        self.initUniverse()
+#        
+#    def initUniverse(self):
+#        for x in range(self.nbrSystemes):#je vais changer ca pour un shuffle plus tard pour eviter les collisions             
+#              self.systemes[x]=Systeme(random.randint(0,50)*10,random.randint(0,50)*10) 
         
     def ajouterJoueur(self,id):
         couleur=self.couleursJoueurs.pop()
@@ -27,3 +29,18 @@ class Univers(object):
         
     def ajouterSysteme(self, systeme):
         self.systemes.append(systeme)
+        
+    def validePositionSysteme(self,testX,testY):
+        valRet=0
+        for i in range(len(self.systemes)):
+            if(testX==self.systemes[i].x and testY==self.systemes[i].y):
+                valRet=1
+        return valRet        
+
+            
+            
+            
+            
+            
+            
+            
