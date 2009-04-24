3b2caa76199ece33d06cdd83121f2fd3f022268e
michel.begoc <michel.begoc@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-04-24 03:41:16 +0000

    Mise à jour de la vue. J'ai fait pas mal de modifications, donc c'est possible que si vous utilisez cette nouvelle version, plus rien ne marche. Au menu: - on peut selectionner des objets sur le canvas - on peut representer des vaisseaux de différentes manieres - j'ai fusionné les deux fichiers de la vue, étant donné que je travaille seul maintenant la dessus - un petit test pour ceux que ça interesse: on peut declencher un evenement sur un clique sur un objet du canvas ;)
    J'ai généré un univers factice et quelques vaisseaux pour mes tests, mais il n'y a aucune liaison avec le controlleur pour le moment.
    
    J'avais besoin de pouvoir identifier les vaisseaux que j'affiche, donc j'ai ajouté un champ nom sur le vaisseau chez moi, mais je n'ai pas modifié le repository afin de ne pas interférer avec le travail d'Eliana. Il va falloir ajouter un champ nom, ou id, ou whatever pour qu'on puisse identifier les vaisseaux et systemes selectionnés.

--- branches/r84/src/client/Vue.py
+++ branches/r84/src/client/Vue.py
@@ -4,9 +4,10 @@ Created on 2009-04-02
 @author: Michel
 '''
 
-from client.Canvas import ZoneDeJeu
 from Tkinter import *
 import random
+import glob, os
+from modele.Vaisseau import Vaisseau
 
 class Vue(object):
     def __init__(self, parent):
@@ -24,6 +25,7 @@ class Vue(object):
         self.blanc = "#fff"
         self.gris = "#778"
         self.mauve = "#90f"
+        self.grille= "#799990"
         
         
         self.zoneJeu = ZoneDeJeu(self)
@@ -46,9 +48,17 @@ class Vue(object):
     
         self.zoneJeu["xscrollcommand"] = self.scrollX.set
         self.zoneJeu["yscrollcommand"] = self.scrollY.set
-
-        if __name__ == "__main__":
-            self.root.mainloop()
+        
+        #tests -- a supprimer des que les controlleurs sont fonctionnels
+        for i in range(200):
+            r = random.Random()
+            self.zoneJeu.dessinerImage("systeme3", r.randint(0, 2000), r.randint(0, 2000), "test")
+        self.zoneJeu.nouveauVaisseauMilitaire(Vaisseau(200, 200))
+        self.zoneJeu.nouveauVaisseauMilitaire(Vaisseau(100, 180))
+        self.zoneJeu.nouveauVaisseauCivil(Vaisseau(210, 230))
+        self.zoneJeu.nouveauVaisseauCivil(Vaisseau(80, 120))
+        self.zoneJeu.nouvelleSonde(600, 500)
+        
     
 class MenuBas(Frame):
     def __init__(self, parent):
@@ -65,6 +75,8 @@ class MenuBas(Frame):
         self.bouton1.pack(side=LEFT)
         self.bouton2.pack(side=LEFT)
         self.bouton3.pack(side=LEFT)
+
+
 
 class Chat(Frame):
     def __init__(self, parent):
@@ -124,6 +136,8 @@ class Chat(Frame):
         
         self.affichage["state"] = disable
 
+
+
 class MenuCote(Frame):
     def __init__(self, parent):
         #appel au constructeur de la super classe
@@ -157,4 +171,151 @@ class MenuCote(Frame):
         
     def systeme(self, evt):
         r = random.Random()
-        self.parent.zoneJeu.dessinerImage("systeme2", r.randint(0, 2000), r.randint(0, 2000))
+        self.parent.zoneJeu.dessinerImage("systeme2", r.randint(0, 2000), r.randint(0, 2000), "test")
+
+
+
+class ZoneDeJeu(Canvas):
+    def __init__(self, parent):
+        '''definition des differents parametres de l'affichage 
+        definitions faites avant l'appel au constructeur car le constructeur a besoin de certaines de ces valeurs'''
+        self.largeurVue = 1000
+        self.hauteurVue = 600
+        self.largeurMonde = 2000
+        self.hauteurMonde = 2000
+        self.largeurGrille = 200
+        self.baseUnites = 5
+        self.itemSelectionne = None
+
+        #appel au constructeur de la super classe
+        Canvas.__init__(self, parent.root, width=self.largeurVue, height=self.hauteurVue, background="#000000", scrollregion=(0, 0, self.largeurMonde, self.hauteurMonde))
+        
+        #on garde une reference vers la classe parent
+        self.parent = parent
+        
+        # rond autour de l'entite selectionne
+        self.rondSelection = None 
+        # x qui represente le cible de deplacement ou d'attaque besoin de 2 lignes
+        self.croix1 = None 
+        self.croix2 = None
+        
+        #initialisation des images disponibles
+        self.images = {}
+        ZoneDeJeu.__genererImages__(self)
+
+        #tracage du cadrillage style "tactique"
+        for i in range(0, self.largeurMonde, self.largeurGrille):
+            self.create_line(0, i, self.largeurMonde, i, fill=self.parent.grille)
+        for i in range(0, self.hauteurMonde, self.largeurGrille):
+            self.create_line(i, 0, i, self.largeurMonde, fill=self.parent.grille)
+
+        #binds des evenements
+        self.bind("<Button-1>", self.gestionClic) #envoie l'objet event au controlleur
+        self.bind("<Button-3>", self.parent.parent.ClickEvent) #envoie l'objet event au controlleur
+
+    #representer l'ensemble des sytemes initiaux
+    def initialiserSystemes(self, systemes):
+        for systeme in systemes:
+            self.dessinerImage("systeme2", systeme.x, systeme.y, systeme.nom)
+        
+    def detruireSysteme(self, systeme):
+        self.delete("systeme.nom")
+        self.dessinerImage("nebuleuse", systeme.x, systeme.y, systeme.nom)
+        
+    def nouveauVaisseauMilitaire(self, vaisseau):
+        x = vaisseau.x
+        y = vaisseau.y
+        l = self.baseUnites*2 #largeur des unites
+        self.create_polygon(x+self.baseUnites, y, x+l, y+l, x, y+l, fill=self.parent.rouge, tags=vaisseau.nom)
+        
+    def nouveauVaisseauCivil(self, vaisseau):
+        x = vaisseau.x
+        y = vaisseau.y
+        l = self.baseUnites*2 #largeur des unites
+        self.create_rectangle(x, y, x+l, y+l, fill=self.parent.vert, tags=vaisseau.nom)
+        self.tag_bind(vaisseau.nom, "<Button-1>", self.testTagBind)
+        
+    def nouvelleSonde(self, x, y):
+        l = self.baseUnites #largeur des unites
+        self.create_oval(x, y, x+l, y+l, fill=self.parent.bleu)
+        
+    def deplacerVaisseau(self, vaisseau):
+        self.coords(vaisseau, vaisseau.x-self.baseUnites, vaisseauy-self.baseUnites, vaisseau.x+self.baseUnites, vaisseau.y+self.baseUnites)
+        
+    def gestionClic(self, event):
+        xy = self.calculerPositionReelle((event.x,event.y))
+        x = xy[0]
+        y = xy[1]
+                
+        #deselection de l'item acutellement selectionne
+        if self.itemSelectionne != None:
+            self.itemconfigure(self.itemSelectionne, outline="")
+        self.itemSelectionne = None
+        
+        #selection d'un ou plusieurs objets
+        objetsCliques = self.find_overlapping(x-self.baseUnites, y-self.baseUnites, x+self.baseUnites, y+self.baseUnites)
+        
+        #on commence par creer une liste avec seulement les objets qui nous interessent
+        objets = []
+        for objet in objetsCliques:
+            if self.type(objet) != "line":
+                objets.append(objet)
+        
+        if len(objets) == 1:
+            type = self.type(objets[0])
+            '''on recupere le premier tag sur l'objet - normalement il doit y en avoir qu'un. Si il y en a plusieurs, il va y avoir des bugs ici
+            A priori, la seule solution qu'il y aurait ce serait de connaitre tous les autres tags que celui qui identifie l'objet et de les tester'''
+            tag = self.gettags(objets[0])
+            print tag[0]# pour le moment on print le nom de l'objet, apres il va falloir l'envoyer au controlleur client pour qu'il connaisse l'objet selectionne
+            if type == "polygon" or type == "oval" or type == "rectangle":
+                print "forme"
+                self.itemconfigure(objets[0], outline=self.parent.blanc)
+                self.itemSelectionne = objets[0]
+            elif type == "image":
+                position = self.coords(objets[0])
+                self.itemSelectionne = self.create_oval(position[0]-self.baseUnites*2, position[1]-self.baseUnites*2, position[0]+self.baseUnites*2, position[1]+self.baseUnites*2, outline=self.parent.blanc)
+        elif len(objets) > 1:
+            print "liste d'objet a ce point" #il va falloir afficher un menu contextuel avec la liste des objets
+
+    '''ce calcule est necessaire, car l'event contient la position du clic sur la fenetre, pas sur le canvas. 
+    Il faut donc convertir cette position en fonction de la valeur de 'scroll' du canvas''' 
+    def calculerPositionReelle(self, xy):
+        x = self.xview()[0]*self.largeurMonde+xy[0]
+        y = self.yview()[0]*self.hauteurMonde+xy[1]
+        return (x,y)
+        
+
+   
+    def drawCroix(self,event):
+        xy = self.calculerPositionReelle((event.x,event.y))
+        x = xy[0]
+        y = xy[1]
+        self.croix1 = self.create_line(x-10, y, x+10, y, fill="green",width=3)
+        self.croix2 = self.create_line(x, y-10, x, y+10, fill="green",width=3)
+        
+    def deleteCroix(self):
+        self.delete(self.croix1)
+        self.delete(self.croix2)
+
+        
+    '''ATTENTION: cette fonction doit imperativement etre appelee a travers la classe de l'objet, par par l'objet lui meme (ZoneDeJeu.__genererImages__(objet) )
+    Je ne sais pas pourquoi, mais si l'image est generee dans l'objet sur lequel elle va etre affichee, ca ne marche pas. Il faut donc appeler cette methode
+    sur la classe et passer en parametre l'instance d'un ZoneDeJeu dans lequel la liste d'images sera cree'''
+    #genere un dictionnaire des images contenues dans le repertoire images. La clef utilisee comme entree du dictionnaire est le nom de base du fichier (sans extension)
+    def __genererImages__(instance):
+        for image in glob.glob("images/*"):
+            photo = PhotoImage(file=image)
+            key = os.path.splitext(os.path.split(image)[1])[0]
+            instance.images[key] = photo
+        
+    #cette fonction ne sert qu'a simplifier le dessin des images
+    def dessinerImage(self, nom, x, y, tag):
+        if nom in self.images:
+            self.create_image(x, y, image=self.images[nom], tags=tag)
+            return 0
+        else:
+            return 1
+    
+    #test de bind evenement sur objet du canvas
+    def testTagBind(self, event):
+        print "tagBind"
