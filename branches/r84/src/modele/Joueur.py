c16a5ae539b9cd2c03afdc3d8e34e33adc1244c9
michel.begoc <michel.begoc@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-05-05 22:04:08 +0000

    La suite des ressources au niveau du joueur.
    Il me reste à faire la différence entre ressources locales et globales.

--- branches/r84/src/modele/Joueur.py
+++ branches/r84/src/modele/Joueur.py
@@ -10,18 +10,26 @@ class Joueur(object):
         self.ajouterSysteme(systInit)
         self.message = []
         self.chatnbrMessage = chatnbrMessage
+        self.ressources = Ressources(10)
         
     def ajouterSysteme(self, systeme):
         self.systemes.append(systeme)      
     
     def ajouterVaisseau(self, posX, posY):
         self.vaisseaux.append(Vaisseau(posX,posY))
-       
-        
-  
-  
-  
-  
-  
-  
-  
+             
+    '''calcul de la production des ressources de maniere globale
+    C'EST RESSOURCES SONT UNIQUEMENT LES RESSOURCES GLOBALES'''
+    def exploiterRessources(self):
+        for systeme in self.systemes:
+            ressources.additionner(systeme.ressourcesProduites())
+    
+    '''calcul la consommation des ressources de maniere globale
+    C'EST RESSOURCES SONT UNIQUEMENT LES RESSOURCES GLOBALES'''
+    def calculerRessourcesConsommees(self):
+        for systeme in self.systemes:
+            ressources.consommer(systeme.calculerRessourcesConsommees())
+        for vaisseau in self.vaisseaux:
+            ressources.consommer(vaisseau.ressourcesEntretien)
+        '''il va falloir potentiellement ajouter ici la consommation pour tout
+         autre objet qui consommerait des ressources'''
