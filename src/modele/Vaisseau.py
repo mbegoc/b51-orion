e9bfcf074accefab33ddc162f90e0cbaef6628f8
michel.begoc <michel.begoc@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-05-08 01:22:04 +0000

    La fonction de deplacement des vaisseaux est réécrite. Les vaisseaux se déplacent linéairement et s'arrête à l'emplacement exact où ils ont été envoyés.

--- src/modele/Vaisseau.py
+++ src/modele/Vaisseau.py
@@ -1,5 +1,6 @@
 from modele.Ressources import Ressources
 from modele.Evenements import *
+import math
 
 class Vaisseau(object):
     def __init__(self, x, y, id="default", classe="civil", type="default"):
@@ -8,11 +9,13 @@ class Vaisseau(object):
         self.etat = "rien" # pour s'avoir s'il attaque ou s'il est mort par exemple
         self.xArrivee = x
         self.yArrivee = y
+        self.vitesse = 5
+        
         self.id = id
         self.type = type
         self.classe = classe
         self.ressourcesEntretien = Ressources(0)
-        self.ressourcesEntretien.gaz = 1
+        self.ressourcesEntretien.gaz = 0
         
         self.ressourcesPropulsion = Ressources(0)
         self.ressourcesPropulsion.gaz = 20
@@ -23,16 +26,45 @@ class Vaisseau(object):
         self.ressourcesPropulsion.consommer(self.ressourcesEntretien)
         if self.x != self.xArrivee or self.y != self.yArrivee:
             if self.ressourcesPropulsion.gaz >= 0:
-                if self.x < self.xArrivee:
-                    self.x = self.x+5
-                elif self.x > self.xArrivee:
-                    self.x = self.x-5
-                    
-                if self.y < self.yArrivee:
-                    self.y = self.y+5
-                
-                elif self.y > self.yArrivee:
-                    self.y = self.y-5
+                if self.xArrivee - self.x == 0:
+                    if self.yArrivee - self.y < 0:
+                        direction = -math.pi/2
+                    else:
+                        direction = math.pi/2
+                else:
+                    direction = math.atan((self.yArrivee - self.y)/(self.xArrivee - self.x))
+                    if self.xArrivee - self.x < 0:
+                        if direction > 0:
+                            direction = -math.pi + direction
+                        else:
+                            direction = math.pi + direction
+
+                #ensuite on calcule la nouvelle position
+                newX = self.vitesse * math.cos(direction)
+                newY = self.vitesse * math.sin(direction)
+
+                #on verifie si on est arrive a destination
+                #si oui, on place le vaisseau a la position d'arrivee (pour eviter de la depasser)
+                #sinon on place le vaisseau a la nouvelle position
+                xActuel = self.x
+                self.x = self.x + int(newX)
+                if self.xArrivee != xActuel:
+                    if (self.xArrivee - xActuel)/math.fabs(self.xArrivee - xActuel) == 1:
+                        if self.x > self.xArrivee:
+                            self.x = self.xArrivee
+                    elif (self.xArrivee - xActuel)/math.fabs(self.xArrivee - xActuel) == -1:
+                        if self.x < self.xArrivee:
+                            self.x = self.xArrivee
+
+                yActuel = self.y
+                self.y = self.y + int(newY)
+                if self.yArrivee != yActuel:
+                    if (self.yArrivee - yActuel)/math.fabs(self.yArrivee - yActuel) == 1:
+                        if self.y > self.yArrivee:
+                            self.y = self.yArrivee
+                    elif (self.yArrivee - yActuel)/math.fabs(self.yArrivee - yActuel) == -1:
+                        if self.y < self.yArrivee:
+                            self.y = self.yArrivee
             else:
                 #on genere un evenement car le vaisseau n'a plus suffisamment de ressources pour se deplacer
                 self.genererEvent("Panne", "Ressources insuffisantes pour se deplacer")
@@ -46,7 +78,7 @@ class Vaisseau(object):
     def removeVaisseauListener(self, ecouteur):
         self.ecouteurs.remove(ecouteur)
     
-    #generation d'un evenement: on cree l'objet evenment, et on le passe à la liste d'ecouteurs inscrits
+    #generation d'un evenement: on cree l'objet evenment, et on le passe a la liste d'ecouteurs inscrits
     #pour s'inscrire, un ecouteur DOIT IMPERATIVEMENT POSSEDER UNE METHODE vaisseauEvent(event)
     def genererEvent(self, type, message, code=0):
         ve = VaisseauEvent(self, type, message, code)
