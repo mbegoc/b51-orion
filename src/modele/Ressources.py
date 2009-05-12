f6d475aad29d0d376c04568408b3d889418b80fd
michel.begoc <michel.begoc@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-05-12 20:24:34 +0000

    Nouvelle ressource ajoutee: production

--- src/modele/Ressources.py
+++ src/modele/Ressources.py
@@ -2,19 +2,22 @@ class Ressources(object):
     def __init__(self, default = 0):
         #les 6 ressources de base
         self.nourriture = default
-        self.connaissance = default
         self.gaz = default
         self.metaux = default
         self.energie = default
         self.credit = default
+        self.connaissance = default
+        self.production = default
+        
         
     def multiplier(self, ressources):
         self.nourriture = self.nourriture * ressources.nourriture
-        self.connaissance = self.connaissance * ressources.connaissance 
         self.gaz = self.gaz * ressources.gaz
         self.metaux = self.metaux * ressources.metaux
         self.energie = self.energie * ressources.energie 
         self.credit = self.credit * ressources.credit
+        self.connaissance = self.connaissance * ressources.connaissance 
+        self.production = self.production * ressources.production
         
         if self.nourriture<0 or self.connaissance<0 or self.gaz<0 or self.energie<0 or self.credit<0 or self.metaux<0:
             return 0
@@ -23,13 +26,14 @@ class Ressources(object):
 
     def additionner(self, ressources):
         self.nourriture = self.nourriture + ressources.nourriture
-        self.connaissance = self.connaissance + ressources.connaissance 
         self.gaz = self.gaz + ressources.gaz
         self.metaux = self.metaux + ressources.metaux
         self.energie = self.energie + ressources.energie 
         self.credit = self.credit + ressources.credit
+        self.connaissance = self.connaissance + ressources.connaissance 
+        self.production = self.production + ressources.production 
         
-        if self.nourriture<0 or self.connaissance<0 or self.gaz<0 or self.energie<0 or self.credit<0 or self.metaux<0:
+        if self.nourriture<0 or self.connaissance<0 or self.gaz<0 or self.energie<0 or self.credit<0 or self.metaux<0 or self.production<0:
             return 0
         else:
             return 1
@@ -37,12 +41,13 @@ class Ressources(object):
     #retrancher des ressources consommees
     def consommer(self, ressources):
         self.nourriture = self.nourriture - ressources.nourriture
-        self.connaissance = self.connaissance - ressources.connaissance 
         self.gaz = self.gaz - ressources.gaz
         self.metaux = self.metaux - ressources.metaux
         self.energie = self.energie - ressources.energie 
         self.credit = self.credit - ressources.credit
-        if self.nourriture<0 or self.connaissance<0 or self.gaz<0 or self.energie<0 or self.credit<0 or self.metaux<0:
+        self.connaissance = self.connaissance - ressources.connaissance 
+        self.production = self.production - ressources.production 
+        if self.nourriture<0 or self.connaissance<0 or self.gaz<0 or self.energie<0 or self.credit<0 or self.metaux<0 or self.production<0:
             return 0
         else:
             return 1
@@ -51,29 +56,31 @@ class Ressources(object):
     def toList(self):
         liste = {}
         liste["nourriture"] = (self.nourriture)
-        liste["connaissance"] = (self.connaissance)
         liste["gaz"] = (self.gaz)
         liste["metaux"] = (self.metaux)
         liste["energie"] = (self.energie)
         liste["credit"] = (self.credit)
+        liste["connaissance"] = (self.connaissance)
+        liste["production"] = (self.production)
         return liste
     
     #duplique l'objet, si on a besoin de faire des calculs sur une ressources sans la modifier
     def copier(self):
         copie = Ressources()
         copie.nourriture = self.nourriture
-        copie.connaissance = self.connaissance 
         copie.gaz = self.gaz
         copie.metaux = self.metaux
         copie.energie = self.energie 
         copie.credit = self.credit
+        copie.connaissance = self.connaissance 
+        copie.production = self.production 
         return copie
     
     #renvoie seulement les ressources globales - pour simplifier le code des calculs de consommation et d'exploitation
     def getRessourcesGlobales(self):
         ressourcesGlobales = Ressources()
-        ressourcesGlobales.connaissance = self.connaissance
         ressourcesGlobales.credit = self.credit
+        ressourcesGlobales.connaissance = self.connaissance
         return ressourcesGlobales
     
     #renvoie seulement les ressources locales - pour simplifier le code des calculs de consommation et d'exploitation
@@ -83,4 +90,5 @@ class Ressources(object):
         ressourcesLocales.nourriture = self.nourriture
         ressourcesLocales.energie = self.energie
         ressourcesLocales.gaz = self.gaz
+        ressourcesLocales.production = self.production
         return ressourcesLocales
