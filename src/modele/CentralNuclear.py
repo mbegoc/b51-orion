d17b6b345b0c4f1023149e81691211a72da96005
edolillo <edolillo@2248ca68-131f-11de-bcad-8f5833b729a1>
2009-06-12 14:27:48 +0000

    Insfraectructure Central Nuclear

---
+++ src/modele/CentralNuclear.py
@@ -0,0 +1,39 @@
+from modele.Ressource import Ressource
+import threading
+
+class CentralNuclear (Ressource):
+    def __init__(self, niveau="basic"):
+        Ressource.__init__(self)
+        self.niveau = niveau
+        self.isFini = true
+        self.infraestructure =Infraestructure ("CentralNuclear")
+                  
+    def developEducation (self):
+        if self.niveau=="basic":
+            self.credit=0
+            self.energie=1
+            calculCentral()
+            evolucioneCentral()
+        if self.niveau=="mediane":
+            self.credit=2
+            self.energie=4
+            calculCentral()
+            evolucioneCentral()
+        if self.niveau=="avance":
+            self.credit=4
+            self.energie=16
+            calculCentral()
+    def calculEducation (self):
+        self.infraestructure.modificateurRessouces=self.credit
+        self.infraestructure.ressourcesEntretien.energie = self.energie
+ 
+    def evolutionerEducation(self):
+        if(self.isFini):
+            self.isFini=false 
+            if self.niveau=="basic":
+                self.niveau="mediane"
+                self.isFini=true
+                ObjTimer=threading.Timer(600.0,EvolutionerEducation)
+                ObjTimer.start()     
+            if self.niveau=="mediane":
+                self.niveau="avance"
